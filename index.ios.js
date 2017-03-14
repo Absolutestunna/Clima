f13acc0da7df81e71aeaa58f75092aa8654f4a78
Joshua Abu <joshujah007@gmail.com>
2017-03-14 15:31:44 -0400

    dynamically changing color of page based on hard coded c value

--- index.ios.js
+++ index.ios.js
@@ -8,59 +8,83 @@ import {
   Button
 } from 'react-native';
 import { getApiKey } from './config/keys';
-
-
-
+import { tempToColor } from './config/weatherColors';
 
 
 export default class WeatherApp extends Component {
   state = {
     initialPosition: {},
     lastPosition: {},
+    temp_c: '28',
+    temp_f: '55',
+    weather: 'Partly Cloudy',
+    viewPageBackgroundColor: {backgroundColor: 'hsl(348, 99%, 100%)'}
   };
 
-  getLocationParams() {
-
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        console.log('position', position);
-        this.setState({initialPosition: position}, () => this.fetchWeatherReportFunc());
-      },
-      (error) => alert(JSON.stringify(error)),
-        { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
-      );
-
-      this.watchID = navigator.geolocation.watchPosition((position) => {
-        this.setState({lastPosition: position});
-      });
+  fetchWeatherInfoByIP() {
+    const { apiKey } = getApiKey();
+    const { baseURL } = this.props;
+    let IPlocationUrl = `${baseURL}/${apiKey}/conditions/q/autoip.json`
+    this.fetchWeatherInfoFunc(IPlocationUrl)
   }
 
-  fetchWeatherReportFunc(){
+  getLocationParams() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({initialPosition: position}, () => this.fetchWeatherInfoByGeoLocation());
+      },
+      (error) => {
+        let { message } = error
+        alert(message)
+        this.fetchWeatherInfoByIP()
+      }
+    );
+  }
+
+  fetchWeatherInfoByGeoLocation(){
     let { initialPosition: { coords: { latitude, longitude } } } = this.state;
     const { apiKey } = getApiKey();
     const { baseURL } = this.props;
-    let url = `${baseURL}/${apiKey}/conditions/q/${latitude},${longitude}.json`;
+    let geoLocationUrl = `${baseURL}/${apiKey}/conditions/q/${latitude},${longitude}.json`
+    this.fetchWeatherInfoFunc(geoLocationUrl);
+  }
+
+  fetchWeatherInfoFunc(url) {
     return fetch(url)
       .then((response) => response.json())
       .then((responseJson) => {
-        console.log('responseJson', responseJson);
-        let { current_observation: { display_location: { city, state } }, temp_c, temp_f, weather } = responseJson;
+        const { current_observation: { display_location: { city, state }, temp_c, temp_f, weather } } = responseJson;
         this.setState({
-          city, state, temp_c, temp_f,
-        })
+          city, state, temp_c, temp_f, weather
+        }, this.handleDisplayPageDetails);
       })
       .catch((error) => {
         console.error(error);
       });
-  }
+   }
+   handleDisplayPageDetails() {
+     let { temp_c } = this.state;
+     let backgroundColor = tempToColor(temp_c);
+     console.log('backgroundColor', backgroundColor);
+     this.setState({
+       viewPageBackgroundColor: { backgroundColor }
+     })
 
-  componentWillUnmount() {
-    navigator.geolocation.clearWatch(this.watchID);
-  }
+   }
+
+   componentDidMount() {
+     this.handleDisplayPageDetails()
+   }
+
+  //
+  // 31 - 44,640 - 58,416
+  // 28 - 53,760
 
   render() {
+    let { viewPageBackgroundColor } = this.state;
+    console.log('viewPageBackgroundColor', viewPageBackgroundColor);
     return (
-      <View style={styles.container}>
+      <View style={[viewPageBackgroundColor, styles.container]}>
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
@@ -81,7 +105,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     flexDirection: 'column',
-    backgroundColor: '#F5FCFF',
   },
   welcome: {
     fontSize: 20,
